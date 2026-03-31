c5a092b8c98e08c0ce1ecb81d72bb382429df296
mohithgit69 <mohithgit@gmail.com>
2026-03-31 17:46:10 +0530

    Remove countdown timer code
    Removed countdown functionality from script.js.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
-// ================= COUNTDOWN =================
 
-const eventDate = new Date("March 30, 2026 09:30:00").getTime();
-
-setInterval(function(){
-
-const now = new Date().getTime();
-const distance = eventDate - now;
-
-const days = Math.floor(distance/(1000*60*60*24));
-const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
-const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
-const seconds = Math.floor((distance%(1000*60))/1000);
-
-document.getElementById("countdown").innerHTML =
-days+"d "+hours+"h "+minutes+"m "+seconds+"s";
-
-},1000);
 
 
 // ================= SCROLL FADE =================
